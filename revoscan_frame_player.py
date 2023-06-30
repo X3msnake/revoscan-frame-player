b566abc775e1aea76dcc4b995e91253cda524c4f
X3msnake <X3msnake@users.noreply.github.com>
2023-06-30 03:52:49 +0100

    Add export to RGB jpg sequence

--- revoscan_frame_player.py
+++ revoscan_frame_player.py
@@ -3,17 +3,33 @@ import cv2
 import tkinter as tk
 from tkinter import filedialog
 import numpy as np
-
+import shutil
 
 print("\n\t ..................................................................... ")
 print("\n\t Welcome to Revoscan frame player, to navigate use the following keys: ")
+print("")
 print("\n\t ESC: quit")
-print("\n\t Shift+E: export")
+print("")
+print("\n\t Shift+E: export as video")
+print("\n\t Shift+X: export as RGB jpg sequence")
 print("\n\t Shift+O: open new cache folder")
+print("")
 print("\n\t Space or Numpad 5: play/stop")
 print("\n\t Numpad 4/6: back / forward 1 frame")
 print("\n\t Numpad 7/9: back / forward 10 frame")
 print("\n\t..................................................................... \n")
+
+class ProgressBar:
+    def __init__(self, total_frames, progress_bar_width):
+        self.total_frames = total_frames
+        self.progress_bar_width = progress_bar_width
+        self.current_frame = 0
+
+    def update(self):
+        self.current_frame += 1
+        progress = int((self.current_frame / self.total_frames) * self.progress_bar_width)
+        progress_bar = '[' + '#' * progress + ' ' * (self.progress_bar_width - progress) + ']'
+        print(f'\r\tExporting Frames: {progress_bar} {self.current_frame}/{self.total_frames}', end='')
 
 def play_image_sequence():
     # Prompt the user to select a folder
@@ -49,6 +65,28 @@ def play_image_sequence():
     output_width, output_height = None, None
     fps = 24.0
 
+    def export_frames_to_folder(folder_path):
+        export_folder = filedialog.askdirectory(title="Select export folder")
+        if not export_folder:
+            return
+
+        progress_bar = ProgressBar(len(image_files), 50)
+
+        for i, img_file in enumerate(image_files):
+            image_path = os.path.join(folder_path, img_file)
+            frame = cv2.imread(image_path)
+
+            # Convert BGR to RGB
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # Export frame as JPEG
+            output_path = os.path.join(export_folder, f'frame_{i+1:04d}.jpg')
+            cv2.imwrite(output_path, frame_rgb)
+
+            progress_bar.update()
+
+        print("\n\tFrames exported successfully.")
+
     while cv2.getWindowProperty(window_name, cv2.WND_PROP_VISIBLE) >= 1:
         # Load the current frame
         image_path = os.path.join(folder_path, image_files[current_frame])
@@ -69,29 +107,32 @@ def play_image_sequence():
         # Play/Pause controls
         if key == ord(' ') or key == ord('5'):
             is_playing = not is_playing
-        
+
         if key == ord('O'):
             cv2.destroyAllWindows()
             play_image_sequence()
-            
+
         # Scrub controls
         # Right cursor key with numpad active
         if key == ord('4') and current_frame > 0:
             current_frame -= 1
-        
+
         # Left cursor key with numpad active
         elif key == ord('6') and current_frame < len(image_files) - 1:
             current_frame += 1
-        
+
         # +-10 frames
         if key == ord('7') and current_frame > 0:
             current_frame -= 10
-        
+
         # Left cursor key with numpad active
         elif key == ord('9') and current_frame < len(image_files) - 1:
             current_frame += 10
 
-        # Export to video if Shift+E is pressed
+        # Export frames to a folder if Shift+X is pressed
+        if key == ord('X'):
+            export_frames_to_folder(folder_path)
+
         # Export to video if Shift+E is pressed
         if key == ord('E'):
             output_file = filedialog.asksaveasfilename(
@@ -114,6 +155,7 @@ def play_image_sequence():
                 # Export frames to video
                 total_frames = len(image_files)
                 progress_bar_width = 50
+                progress_bar = ProgressBar(total_frames, progress_bar_width)
 
                 for i, img_file in enumerate(image_files):
                     image_path = os.path.join(folder_path, img_file)
@@ -123,9 +165,7 @@ def play_image_sequence():
                     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
                     # Display progress
-                    progress = int((i + 1) / total_frames * progress_bar_width)
-                    progress_bar = '[' + '#' * progress + ' ' * (progress_bar_width - progress) + ']'
-                    print(f'\r\tExporting Frames: {progress_bar} {i + 1}/{total_frames}', end='')
+                    progress_bar.update()
 
                     output_video.write(frame_rgb)
 
