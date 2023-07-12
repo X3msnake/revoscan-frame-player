874bc87230ee7af6cb61f9af7f4ea96ac64635e3
X3msnake <X3msnake@users.noreply.github.com>
2023-07-12 16:22:54 +0100

    Cleanup variable names and more user info

--- helpers/dph_display.py
+++ helpers/dph_display.py
@@ -13,13 +13,17 @@ dphfilepath = "frame_000_0000.dph"
 #################################################
 
 # Image byte size checker
+# Check your image size and then shape it
+# accordingly. Example: 1048576=1024*1024
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 img = np.fromfile(dphfilepath, dtype=np.uint16)
-print (img.size) #check your image size, say 1048576
-#shape it accordingly, that is, 1048576=1024*1024
+print (img.size) 
 
+
+#################################################
 
 # Revoscan DPH file decoder
 import numpy as np
@@ -38,19 +42,20 @@ raw_image = raw_image.reshape(height, width)
 # Perform histogram equalization
 equalized_image = cv2.equalizeHist(np.uint8(raw_image / (np.max(raw_image) / 255.0)))
 
-# Get the Jet colormap
+# Get the colormap, check the link for other available templates
 # https://docs.opencv.org/4.x/d3/d50/group__imgproc__colormap.html
-jet_colormap = cv2.applyColorMap(np.arange(256, dtype=np.uint8), cv2.COLORMAP_SUMMER)
+colormap = cv2.applyColorMap(np.arange(256, dtype=np.uint8), cv2.COLORMAP_SUMMER)
 
 # Reverse the colormap array
-flipped_jet_colormap = jet_colormap[::-1]
-#flipped_jet_colormap = jet_colormap
+flipped_colormap = colormap[::-1]
+#flipped_colormap = colormap
 
 # Apply the flipped Jet colormap to the equalized image
-colormap_image = cv2.applyColorMap(equalized_image, flipped_jet_colormap)
+colormap_image = cv2.applyColorMap(equalized_image, flipped_colormap)
 
 # Create a window and display the image
 cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('Image', width, height)
 cv2.imshow('Image', colormap_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
