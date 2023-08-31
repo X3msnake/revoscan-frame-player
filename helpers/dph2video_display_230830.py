1e5a13a96cfcc6c409af4729d9a795160a682b42
X3msnake <X3msnake@users.noreply.github.com>
2023-09-01 00:38:28 +0100

    Add low rez scan modes (Fast)
    https://chat.openai.com/share/ce7b2d74-5552-429a-8c4f-da2cd5f908e8

--- helpers/dph2video_display_230830.py
+++ helpers/dph2video_display_230830.py
@@ -12,18 +12,36 @@ if not dph_files:
     input("\n\t Press enter to exit;")
     exit()
 
+
+
 print("\n\t................................................................................")
 print("\n\t                                                                                ")
 print("\n\t   ESC: exits the script                                                        ")
 print("\n\t                                                                                ")
 print("\n\t - This script loops trough the dph files in the same folder as this py.        ")
 print("\n\t - Video is being saved while you see it run and is output to dph_output.mp4    ")
-print("\n\t - The file will be in the same folder as this script                           ")
+print("\n\t - The file will be in the same folder as this script                         \n")
 print("\n\t..............................................................................\n")
 
 # Define width and height for the output video frames
-width = 640
-height = 400
+img = np.fromfile(dph_files[0], dtype=np.uint16)
+img_size = img.size
+
+target_sizes = {
+    64000: (320, 200),
+    256000: (640, 400)
+}
+
+if img_size in target_sizes:
+    width, height = target_sizes[img_size]
+else:
+    # Calculate width and height based on the image size
+    # You can adjust this calculation according to your requirements
+    ratio = img_size / 64000  # Adjust the divisor as needed
+    width = int(320 * ratio)   # Adjust the base width as needed
+    height = int(200 * ratio)  # Adjust the base height as needed
+
+print(f" \t   Raw depth capture: {width}x{height}")
 
 # Create a VideoWriter object to save the output video
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
